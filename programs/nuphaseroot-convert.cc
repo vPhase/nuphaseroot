225c5618bbd07ae3d4d4f2f0dc3fe72cdd627d96
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2018-02-12 19:11:08 -0600

    fix is dir logic and add n processed

--- programs/nuphaseroot-convert.cc
+++ programs/nuphaseroot-convert.cc
@@ -35,32 +35,34 @@ int main (int nargs, char ** args)
   {
     //check if we have a directory
     struct stat sb; 
-    if (stat(args[2],&sb) && S_ISDIR(sb.st_mode))
+    if (!stat(args[2],&sb) && S_ISDIR(sb.st_mode))
     {
+      printf("It's a dir!\n"); 
       is_dir = true; 
     }
   }
 
 
+  int nproc = 0;
   if (!strcasecmp(what,"hk"))
   {
-    if (is_dir) nuphase::convert::convertHkDir(args[2],args[3]);
-    else nuphase::convert::convertHkFiles(ninputs, (const char **) args+2, args[nargs-1]); 
+    if (is_dir) nproc = nuphase::convert::convertHkDir(args[2],args[3]);
+    else nproc = nuphase::convert::convertHkFiles(ninputs, (const char **) args+2, args[nargs-1]); 
   }
   else if (!strcasecmp(what,"status"))
   {
-    if (is_dir) nuphase::convert::convertStatusDir(args[2],args[3]);
-    else nuphase::convert::convertStatusFiles(ninputs, (const char **) args+2, args[nargs-1]); 
+    if (is_dir) nproc = nuphase::convert::convertStatusDir(args[2],args[3]);
+    else nproc = nuphase::convert::convertStatusFiles(ninputs, (const char **) args+2, args[nargs-1]); 
   }
   else if (!strcasecmp(what,"header"))
   {
-    if (is_dir) nuphase::convert::convertHeaderDir(args[2],args[3]);
-    else nuphase::convert::convertHeaderFiles(ninputs, (const char **) args+2, args[nargs-1]); 
+    if (is_dir) nproc = nuphase::convert::convertHeaderDir(args[2],args[3]);
+    else nproc = nuphase::convert::convertHeaderFiles(ninputs, (const char **) args+2, args[nargs-1]); 
   }
   else if (!strcasecmp(what,"event"))
   {
-    if (is_dir) nuphase::convert::convertEventDir(args[2],args[3]);
-    else nuphase::convert::convertEventFiles(ninputs, (const char **) args+2, args[nargs-1]); 
+    if (is_dir) nproc = nuphase::convert::convertEventDir(args[2],args[3]);
+    else nproc = nuphase::convert::convertEventFiles(ninputs, (const char **) args+2, args[nargs-1]); 
   }
   else
   {
@@ -68,6 +70,7 @@ int main (int nargs, char ** args)
     return 1; 
   }
 
+  printf("Processed %d items\n", nproc); 
 
   return 0; 
 }
