c6f9d9b3a31322ad9646c0c220d28645fa33a91e
Daniel Lewis Smith <danielsmith@midway2-login2.rcc.local>
2018-07-20 11:52:49 -0500

    Adding cable length and associated delay

--- src/CalibrationInfo.cc
+++ src/CalibrationInfo.cc
@@ -7,6 +7,14 @@ nuphase::CalibrationInfo::CalibrationInfo()
   time_calibration = 1/1.5;
   t = UNITS_NS; 
 
+  nuphase_fiber_delay = 4.880; //ns/m
+
+  
+  const static double temp_len[k::num_boards][k::num_chans_per_board] = {{2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 10.0},
+									{12.0, 14.0, 1.0, 10.0, 0.0, 0.0, 0.0, 0.0}};
+  std::copy(temp_len, temp_len+sizeof(temp_len)/sizeof(temp_len[0]), nuphase_fiber_length);
+
+
   for (int board = 0; board < k::num_boards; board++)
   {
     for (int chan = 0; chan < k::num_chans_per_board; chan++)
