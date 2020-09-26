421285fcedd0250d7660ef561238586ec027b7b8
Cosmin Deaconu <cozzyd@gmail.com>
2020-09-26 15:08:39 -0500

    updated python reader example
     - automatically uses NUPHASE_ROOT_DATA (but pay attention to change in order of positional arguments)
     - uses getters so getting status works in ROOT 6.22/02
     - more efficient waveform access without intermediate TGraph
     - fix a typo

--- examples/nuphase_data_reader.py
+++ examples/nuphase_data_reader.py
@@ -15,6 +15,7 @@ from __future__ import division, print_function
 import ROOT
 import numpy 
 import sys
+import os 
 
 
 ## You need to load the libnuphaseroot library
@@ -26,7 +27,7 @@ ROOT.gSystem.Load("libnuphaseroot.so");
 
 ## A class to read in data from a run
 ##  
-##   e.g. d = nuphase_data_reader("/path/to/runs",runno) 
+##   e.g. d = nuphase_data_reader(runno)  # if $NUPHASE_ROOT_DATA isn't defined, you can pass base_dir as second argument
 ##
 ##   To select the 100th entry in the run
 ##     d.setEntry(65079); 
@@ -48,25 +49,27 @@ ROOT.gSystem.Load("libnuphaseroot.so");
 
 class Reader:
 
-  def __init__(self,base_dir, run):
-
-    self.run = run; 
+  def __init__(self, run, base_dir=None):
+    
+    if base_dir == None: 
+        base_dir = os.environ["NUPHASE_ROOT_DATA"] 
+    self.run = int(run); 
     self.base_dir = base_dir
 
-    self.event_file = ROOT.TFile("%s/run%d/event.root" % (base_dir, run))
+    self.event_file = ROOT.TFile.Open("%s/run%d/event.root" % (base_dir, run))
     self.event_tree = self.event_file.Get("event") 
     self.evt = ROOT.nuphase.Event() 
     self.event_entry = -1; 
     self.event_tree.SetBranchAddress("event",ROOT.AddressOf(self.evt))
 
-    self.head_file = ROOT.TFile("%s/run%d/header.root" % (base_dir, run))
+    self.head_file = ROOT.TFile.Open("%s/run%d/header.root" % (base_dir, run))
     self.head_tree = self.head_file.Get("header") 
     self.head = ROOT.nuphase.Header(); 
     self.head_entry = -1
     self.head_tree.SetBranchAddress("header",ROOT.AddressOf(self.head))
     self.head_tree.BuildIndex("header.event_number") 
 
-    self.status_file = ROOT.TFile("%s/run%d/status.root" % (base_dir, run))
+    self.status_file = ROOT.TFile.Open("%s/run%d/status.root" % (base_dir, run))
     self.status_tree = self.status_file.Get("status") 
     self.stat= ROOT.nuphase.Status(); 
     self.status_tree.SetBranchAddress("status",ROOT.AddressOf(self.stat)) 
@@ -93,10 +96,13 @@ class Reader:
 
 
   def wf(self,ch = 0):  
+
+    bd = ROOT.nuphase.BOARD_MASTER;
+    if (ch > 8) :
+        ch-=8
+        bd = ROOT.nuphase.BOARD_SLAVE
     ## stupid hack because for some reason it doesn't always report the right buffer length 
-    g = self.event().getGraph(ch % 8, ch//8) 
-    return numpy.frombuffer(g.GetY(), numpy.dtype('float64'), self.event().getBufferLength()) - 64 
-    g = None #try to forget 
+    return numpy.frombuffer(self.event().getData(ch,bd), numpy.dtype('float64'), self.event().getBufferLength()) - 64 
 
   def t(self):
     return numpy.linspace(0, self.event().getBufferLength() /1.5, self.event().getBufferLength()) 
@@ -109,7 +115,7 @@ class Reader:
 
   def status(self,force_reload = False): 
     if (self.status_entry != self.current_entry or force_reload): 
-      self.status_tree.GetEntry(self.status_tree.GetEntryNumberWithBestIndex(self.header().readout_time[0], self.header().readout_time_ns[0]))
+      self.status_tree.GetEntry(self.status_tree.GetEntryNumberWithBestIndex(self.header().getReadoutTime(), self.header().getReadoutTimeNs()))
       self.status_entry = self.current_entry
 
     return self.stat
@@ -126,7 +132,7 @@ if __name__=="__main__":
   import matplotlib.pyplot as plt
 
 # If your data is elsewhere, pass it as an argument
-  datapath = sys.arvg[1] if len(sys.argv) > 1 else "/project2/avieregg/nuphase/telem/root"
+  datapath = sys.argv[1] if len(sys.argv) > 1 else os.environ["NUPHASE_ROOT_DATA"]
 
 # look at run 360
   d = Reader(datapath,360) 
